21e05f8a491d2e14c4e23b8a15b2a2fa5d2b7e6b
bgurtz <bgurtz@gmail.com>
2019-11-24 23:05:47 -0600

    " Flutter Course Projects & Examples - Inital Commit "

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 // This is our blank starting point for the course projects
 // Welcome to Flutter / Dart
 //
+// Look through the branches for each of the projects
+// Each project branch branch has an animated gif
+// to show the result of that project.
+//
+// Thank you. 
 // Brian Gurtz
